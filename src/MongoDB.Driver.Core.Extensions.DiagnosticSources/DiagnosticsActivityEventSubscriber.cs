666005c0eed305e9349a8faf13316ddb75e211f9
Lucas Dell'Isola <lucas.dellisola@rystadenergy.com>
2023-05-09 10:46:24 +0200

    Added option to dynamically enable command text based on other command properties

--- src/MongoDB.Driver.Core.Extensions.DiagnosticSources/DiagnosticsActivityEventSubscriber.cs
+++ src/MongoDB.Driver.Core.Extensions.DiagnosticSources/DiagnosticsActivityEventSubscriber.cs
@@ -72,7 +72,7 @@ namespace MongoDB.Driver.Core.Extensions.DiagnosticSources
                     break;
             }
 
-            if (activity.IsAllDataRequested && _options.CaptureCommandText)
+            if (activity.IsAllDataRequested && (_options.CaptureCommandText || (_options.ShouldCaptureCommandText is not null && _options.ShouldCaptureCommandText.Invoke(@event))))
             {
                 activity.AddTag("db.statement", @event.Command.ToString());
             }
--- src/MongoDB.Driver.Core.Extensions.DiagnosticSources/InstrumentationOptions.cs
+++ src/MongoDB.Driver.Core.Extensions.DiagnosticSources/InstrumentationOptions.cs
@@ -6,6 +6,7 @@ namespace MongoDB.Driver.Core.Extensions.DiagnosticSources
     public class InstrumentationOptions
     {
         public bool CaptureCommandText { get; set; }
+        public Func<CommandStartedEvent, bool> ShouldCaptureCommandText { get; set; }
         public Func<CommandStartedEvent, bool> ShouldStartActivity { get; set; }
     }
 }
